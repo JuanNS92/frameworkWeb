bec6365046f5483ae13075db92ba5f235dfadf20
Juan Soares <juan160192@gmail.com>
2020-10-28 21:45:48 -0300

    configuração do navegador

--- FrameWorkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameWorkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,14 +1,30 @@
 package br.com.framework.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Metodos {
-	
-	
+
 	WebDriver driver;
-	
+
 	public void navegador(String url, String navegador) {
-		
+
+		switch (navegador) {
+
+		case "Chrome":
+			System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get(url);
+			break;
+
+		case "Firefox":
+			System.setProperty("webdriver.gecko.driver", "C:\\driver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			driver.get(url);
+			break;
+		}
+
 	}
 
 }
